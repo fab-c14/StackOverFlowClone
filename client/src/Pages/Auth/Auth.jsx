6bf3eb8d2c6c4946d79b2acadaa4f1c1356053c5
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-03 10:32:22 +0000

    Day 11 3rd Commit

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import icon from '../../assets/sta'
+import icon from "../../assets/icon.png";
 function Auth() {
   // use state variable
   const [isSignup, setIsSignup] = useState(false);
@@ -7,8 +7,29 @@ function Auth() {
     <section className='auth-section'>
       <div className='auth-container'>
         {!isSignup && (
-          <img src={icon} alt='stackoverflow' className='login-logo' />
+          <img
+            src={icon}
+            alt='stackoverflow'
+            className='login-logo'
+            width={"30%%"}
+          />
         )}
+        <form>
+          <label htmlFor='email'>
+            <h4> Email </h4>
+            <input type='email' name='name' id='email' />
+          </label>
+          <label htmlFor='password'>
+            <div>
+              <h4> Password </h4>
+              <h4>Forget Password</h4>
+            </div>
+            <input type='password' name='password' id='password' />
+          </label>
+          <button type='submit' className='auth-btn'>
+            {isSignup ? "Signup" : "Login"}
+          </button>
+        </form>
       </div>
     </section>
   );
