d8a47b82eb44ae7369dfa372636d1d6fbfa972ea
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-04 07:17:27 +0000

    Day 12 1st commit

--- client/src/Pages/Auth/Auth.jsx
+++ client/src/Pages/Auth/Auth.jsx
@@ -3,6 +3,9 @@ import icon from "../../assets/icon.png";
 function Auth() {
   // use state variable
   const [isSignup, setIsSignup] = useState(false);
+  const handleSwitch = () => {
+    setIsSignup(!isSignup);
+  };
   return (
     <section className='auth-section'>
       <div className='auth-container'>
@@ -15,6 +18,13 @@ function Auth() {
           />
         )}
         <form>
+          {isSignup && (
+            <label htmlFor='name'>
+              <h4>Display Name</h4>
+              <input type='text' id='name' name='name' />
+            </label>
+          )}
+
           <label htmlFor='email'>
             <h4> Email </h4>
             <input type='email' name='name' id='email' />
@@ -22,9 +32,16 @@ function Auth() {
           <label htmlFor='password'>
             <div>
               <h4> Password </h4>
-              <h4>Forget Password</h4>
+              {!isSignup && <h4>Forget Password</h4>}
             </div>
             <input type='password' name='password' id='password' />
+
+            {isSignup && (
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+                hic atque possimus.
+              </p>
+            )}
           </label>
           <button type='submit' className='auth-btn'>
             {isSignup ? "Signup" : "Login"}
@@ -32,7 +49,11 @@ function Auth() {
         </form>
         <p>
           {isSignup ? "already have an account?" : "Don't have an account?"}
-          <button type='button' className='handle-switch-btn' onClick={handleSwitch}>
+          <button
+            type='button'
+            className='handle-switch-btn'
+            onClick={handleSwitch}
+          >
             {isSignup ? "Login" : "Signup"}
           </button>
         </p>
