3dcd0a5c243acf4cbee30c97b9bf723ce0af7bba
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-12 11:15:34 +0000

    Day 16 2nd commit

--- client/src/components/RightSidebar/WidgetTags.jsx
+++ client/src/components/RightSidebar/WidgetTags.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
 
 function WidgetTags() {
+  const tags = [
+    "c",
+    "css",
+    "c++",
+    "express",
+    "firebase",
+    "python",
+    "php",
+    "reactjs",
+    "html",
+    "js",
+    "json",
+  ];
+
   return (
-    <div>
-      
+    <div className='widget-tags'>
+      <h3>Watched Tags</h3>
+      <div className='widget-tags-div'>{tags.map((tag) => (
+        <p key={tag}>{tag}</p>
+      ))}</div>
     </div>
-  )
+  );
 }
 
-export default WidgetTags
+export default WidgetTags;
