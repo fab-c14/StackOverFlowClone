60a0f8595ae5a07218a7517019f719fe8a3dad70
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-12 10:06:17 +0000

    Day 16 first commit

---
+++ client/src/components/RightSidebar/Widget.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FaPen } from "react-icons/fa";
+import { FaComment } from "react-icons/fa";
+function Widget() {
+  return <div></div>;
+}
+
+export default Widget;
