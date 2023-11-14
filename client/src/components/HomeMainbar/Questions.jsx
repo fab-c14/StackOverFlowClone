35ee701d0894282b154ce939383413b2e9b615e3
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-14 13:47:43 +0000

    Day 17 second commit

--- client/src/components/HomeMainbar/Questions.jsx
+++ client/src/components/HomeMainbar/Questions.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Questions = ({ question }) => {
   return (
-    <div className='display-ans-container'>
+    <div className='display-question-container'>
       <div className='display-votes-ans'>
         <p>{question.votes}</p>
         <p>votes</p>
@@ -15,6 +15,19 @@ const Questions = ({ question }) => {
         <Link to={`/Questions/${question.id}`} className='question-title-link'>
           {question.questionTitle}
         </Link>
+        <div className='display-tags-time'>
+          <div className='display-tags'>
+            {question.questionTags.map((tag) => (
+              <p key={tag}>{tag}</p>
+            ))}
+          </div>
+
+          <p className='display-time'>
+            asked
+            {question.askedOn}
+            {question.userPosted}
+          </p>
+        </div>
       </div>
     </div>
   );
