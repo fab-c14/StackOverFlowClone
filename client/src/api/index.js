7d47256d3b5b112af7f4beab6f79adc4a769322d
Fab-c <71762204+fab-c14@users.noreply.github.com>
2023-11-26 14:27:00 +0000

    Day 24 resolving bug

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API = axios.create({ baseURL: "http://localhost:5000" });
 
-
 export const signUp = (authData) => {
   // Add return statement here
   return API.post("/user/signup", authData);
--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,9 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import Reducers from './reducers';
+import Reducers from "./reducers";
 
-const store = createStore(
-  Reducers,
-  compose(applyMiddleware(thunk))
-);
+const store = createStore(Reducers, compose(applyMiddleware(thunk)));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
